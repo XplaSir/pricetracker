976f1a390136e44cb46460e34d28e3eb639517b5
Nigel Mafukidze <ntmafukidze@gmail.com>
2019-05-17 12:28:15 +0200

    swagger

---
+++ tracker/urls.py
@@ -0,0 +1,17 @@
+
+
+from django.urls import path
+from . import views
+
+
+from rest_framework_swagger.views import get_swagger_view
+
+app_name = 'tracker'
+
+
+urlpatterns = [
+    path('categories', views.CategoryView.as_view(), name='categories'),
+    path('products', views.ProductsView.as_view(), name='products'),
+    path('pricetracker', views.PriceTrackerView.as_view(), name='pricetracker'),
+    path('shops', views.ShopView.as_view(), name='shops'),
+]
--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
+from .models import Category, Product, PriceTracker, Shop
+from .serializers import CategorySerializer, ProductSerializer, PriceTrackerSerializer, ShopSerializer
+from rest_framework import status
+from rest_framework.response import Response
+
+from rest_framework.views import APIView
 
 # Create your views here.
+
+
+class CategoryView(APIView):
+    def get(self, request):
+        '''
+        returns all product categories
+        '''
+
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data)
+
+
+class ProductsView(APIView):
+    def get(self, request):
+        '''
+        returns all products 
+        '''
+
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+
+class PriceTrackerView(APIView):
+    def get(self, request):
+        '''
+        returns pricetracker
+        '''
+        price_tracker = PriceTracker.objects.all()
+        serializer = PriceTrackerSerializer(price_tracker, many=True)
+        return Response(serializer.data)
+
+
+class ShopView(APIView):
+    def get(self, request):
+        '''
+        returns all shops
+        '''
+        shops = Shop.objects.all()
+        serializer = ShopSerializer(shops, many=True)
+        return Response(serializer.data)
